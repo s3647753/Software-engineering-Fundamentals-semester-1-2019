d3e3e42e7743cb7fad2abdff490b86cd5c0dbb6a
rmit-s3652908-shaun-d <s3652908@student.rmit.edu.au>
2019-03-20 13:33:22 +1100

    Add Login class

---
+++ chessGame_v1.0/src/model/Login.java
@@ -0,0 +1,12 @@
+package src.model;
+
+/*
+ * this class takes player name and password,
+ * and checks that:
+ * 1. the player name exists in the players file
+ * 2. the given password's hash matches the corresponding one in the file
+ */
+
+public class Login {
+
+}
