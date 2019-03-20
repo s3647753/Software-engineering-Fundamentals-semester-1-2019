dd06b9fc9ff4d0582d21169502be3dedfc454dc1
rmit-s3652908-shaun-d <s3652908@student.rmit.edu.au>
2019-03-20 13:15:42 +1100

    Add Register class, test commit from Eclipse

---
+++ chessGame_v1.0/src/model/Register.java
@@ -0,0 +1,12 @@
+package src.model;
+
+/*
+ * this class can handle registering players.
+ * takes player name + password;
+ * checks player name is not already taken + doesn't include weird characters (no spaces, maybe?)
+ * saves player name and password hash to file!
+ */
+
+public class Register {
+
+}
