265c2f6b040de5669703b8ff118341d5dd0e752f
rmit-s3652908-shaun-d <s3652908@student.rmit.edu.au>
2019-03-25 17:56:21 +1100

    made static cos hfghfvshdvsdg

--- chessGame_v1.0/src/model/Register.java
+++ chessGame_v1.0/src/model/Register.java
@@ -1,15 +1,24 @@
-package src.model;
+package model;
 
 /*
  * this class can handle registering players.
  * takes player name + password;
  * checks player name is not already taken + doesn't include weird characters (no spaces, maybe?)
  * saves player name and password hash to file!
- * 
- * create a DuplicateNameException class to handle duplicate names
- * in the Playersfile!
+ *
  */
 
 public class Register {
+	
+	/**
+	 * adds a player to the registered players file with the given username and password.
+	 * @param username
+	 * @param password
+	 * @return
+	 */
+	public static boolean Register(String username, String password) throws DuplicateNameException {
+		
+		return false;
+	}
 	
 }
