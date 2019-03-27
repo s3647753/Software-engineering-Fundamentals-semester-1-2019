d0a1fcc69b56a28234521fd1ba15f922472f660c
rmit-s3652908-shaun-d <s3652908@student.rmit.edu.au>
2019-03-27 13:29:42 +1100

    SHA256 hashing for passwords functional, hooray

--- chessGame_v1.0/src/model/Login.java
+++ chessGame_v1.0/src/model/Login.java
@@ -1,15 +1,16 @@
 package model;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+
+import javax.xml.bind.DatatypeConverter;
+
 import model.PlayerNotFoundException;
 
 /**
- * this class takes player name and password,
- * and checks that:
- * 1. the player name exists in the players file
- * 2. the given password's hash matches the corresponding one in the file
- * 
- * this class stores the list of currently logged in players.
+ * this class handles all login functions, and stores the list of currently logged in players.
  * 
  * @author Shaun Davis
  * 
@@ -19,44 +20,80 @@ public class Login {
 	
 	private ArrayList<String> playerList;
 	
+	/**
+	 * the constructor for the Login class simply initalizes the playerList.
+	 * @author Shaun Davis
+	 */
 	public Login() {
 		playerList = new ArrayList<String>();
 	}
 	
 	/**
-	 * hashes the given String with the SHA256 algorithm, because MD5 is for chumps.
-	 * @param inputString
-	 * @return the hashed String.
-	 */
-	private String stringToSHA256(String inputString) {
-		String hashString = null;
-		// i have no idea how hashes work yet
-		return hashString;
-	}
-	
-	/**
-	 * checks if the player ID is present in the players file.
-	 * 
-	 * @return true if the ID was found, false if it was not.
+	 * hashes the given String with the SHA256 algorithm, because MD5 is for chumps. this method is static so that
+	 * the Register class can also make use of it without instantiating Login.
+	 * @param inputString the String to convert to a SHA256 hash
+	 * @return the hashed String
 	 * @author Shaun Davis
 	 */
-	private boolean playerExists(String username) throws PlayerNotFoundException {
+	protected static String stringToSHA256(String inputString) {
+		String hashString = null;
+		
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			// update/populate the digest with the bytes of the inputString
+			// we specify UTF-8, just in case it tries to use something else.
+			md.update(inputString.getBytes(StandardCharsets.UTF_8));
+			byte[] hash = md.digest();
+			hashString = DatatypeConverter.printHexBinary(hash);
+		} catch (NoSuchAlgorithmException e) {
+			// this should never happen
+		}
+		
+		return hashString;
+	}
+
+	/**
+	 * checks that a player exists in the registered players file.
+	 * 
+	 * @param username the username to search for
+	 * @return true if the player exists, false if it doesn't
+	 * @author Shaun Davis
+	 */
+	private boolean playerExists(String username) {
 		// open the players file
 		// search for the username
 		// return true if it's there!
 		return false;
 	}
 	
+	/**
+	 * logs this player into the game by adding their name to the playerList array of this Login instance.
+	 * 
+	 * @param username the player's username
+	 * @param password the player's password in plaintext
+	 * @return true if the login was successful, false if the password was incorrect
+	 * @throws PlayerNotFoundException if the player isn't registered i.e. the username wasn't found in the registered players file
+	 * @author Shaun Davis
+	 */
 	protected boolean loginPlayer(String username, String password) throws PlayerNotFoundException {
 		
 		if (playerExists(username)) {
 			// verify their password hash matches the one in the players file
 			// if so, append the player username to playerList
+			// return true!
+		}
+		else {
+			throw new PlayerNotFoundException();
 		}
 
 		return false;
 	}
 	
+	/**
+	 * returns the playerList!!!!!!!!!!!!!!!!!!!!!
+	 * @return the playerList!!!!!!!!!!!!!!!!!
+	 * @author Shaun Daviasdaagfasd
+	 */
 	protected ArrayList<String> getPlayerList() {
 		return playerList;
 	}
