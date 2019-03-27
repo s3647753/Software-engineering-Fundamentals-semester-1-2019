6bb33050bc101049e0d050c6bc5d783a60becab4
rmit-s3652908-shaun-d <s3652908@student.rmit.edu.au>
2019-03-27 13:30:18 +1100

    not much really

--- chessGame_v1.0/src/model/Register.java
+++ chessGame_v1.0/src/model/Register.java
@@ -1,22 +1,26 @@
 package model;
 
-/*
- * this class can handle registering players.
+/**
+ * this class handles registering players.
  * takes player name + password;
  * checks player name is not already taken + doesn't include weird characters (no spaces, maybe?)
  * saves player name and password hash to file!
+ * 
+ * @author Shaun Davis
  *
  */
 
 public class Register {
 	
 	/**
-	 * adds a player to the registered players file with the given username and password.
-	 * @param username
-	 * @param password
-	 * @return
+	 * adds a player to the registered players file with the given username and password. this is intended to be called without
+	 * instantiating, i.e. just call Register.registerPlayer(username, password).
+	 * @param username the propositioned username
+	 * @param password the password to use, in plaintext. it's converted to a hash before being saved to the registered players file.
+	 * @return true if the player was successfully registered, false if not (for reasons other than a DuplicateNameException)
+	 * @throws DuplicateNameException when the propositioned username is already present in the registered players file
 	 */
-	public static boolean Register(String username, String password) throws DuplicateNameException {
+	public static boolean registerPlayer(String username, String password) throws DuplicateNameException {
 		
 		return false;
 	}
