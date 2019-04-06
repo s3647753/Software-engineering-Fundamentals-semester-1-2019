45cabbe618ea950bf6902ea6a0af76d32ec984d9
s3716853 <41184784+s3716853@users.noreply.github.com>
2019-04-06 22:51:44 +1100

    Exception Handling and setColr method
    Methods with exception handling:
    register
    login
    movePlayer

--- chessGame_v1.0/src/model/GameEngineImpl.java
+++ chessGame_v1.0/src/model/GameEngineImpl.java
@@ -23,7 +23,9 @@ import view_interfaces.View;
 public class GameEngineImpl implements GameEngine {
 	private Board board;
 	private Map<String, Integer> players = new HashMap<String, Integer>();
-	private Collection<View> views = new HashSet<>();
+	//TODO CREATE PLAYER CLASS
+	private Map<Colr, String> playerColr = new HashMap<Colr, String>();
+	private View view;
 	private int currentMove;
 	private Login login;
 	private Colr currentTurn;
@@ -36,14 +38,13 @@ public class GameEngineImpl implements GameEngine {
 
 	@Override
 	public void setView(View view) {
-		views.add(view);
+		this.view = view;
 	}
 
 	@Override
 	public void updateScore(String Username, int score) {
-		if(players.containsKey(Username)) {
-			players.put(Username, score);
-		}
+		players.replace(Username, score);
+		view.update(board);
 	}
 
 	@Override
@@ -63,14 +64,15 @@ public class GameEngineImpl implements GameEngine {
 		 	Does not check what the piece is moving onto yet*/
 		List<Piece> pieces = board.getPiecesAt(from);
 		boolean moveSuccessful = false;
+		int piecesTaken = 0;
 		try {
 			if(pieces.size()>1) {
-				board.moveMergedPiece(from, to);
+				piecesTaken = board.moveMergedPiece(from, to);
 				moveSuccessful = true;
 				reduceMoves();
 				swapTurn();
 			}else if(pieces.size() == 1) {
-				board.moveSinglePiece(pieces.get(0), from, to);
+				piecesTaken = board.moveSinglePiece(pieces.get(0), from, to);
 				moveSuccessful = true;
 				reduceMoves();
 				swapTurn();
@@ -80,6 +82,7 @@ public class GameEngineImpl implements GameEngine {
 		}catch(PieceNotFoundException e) {
 			moveSuccessful = false;
 		}
+		increaseScore(piecesTaken);
 		if(currentMove == 0) {
 			endGame();
 		}
@@ -100,27 +103,45 @@ public class GameEngineImpl implements GameEngine {
 	}
 
 	@Override
-	public String register(String username, String password) throws DuplicateNameException {
-		Boolean registerSuccess = Register.registerPlayer(username, password);
+	public String register(String username, String password) {
+		Boolean registerSuccess = null;
 		String message;
-		if(registerSuccess) {
-			message = "Username " + username + " has successfully registered";
-		}else {
-			message = "Username " + username + " has not successfully registered";
+		try {
+			registerSuccess = Register.registerPlayer(username, password);
+			if(registerSuccess) {
+				message = "Username " + username + " has successfully registered";
+			}else {
+				message = "Username " + username + " has not successfully registered";
+			}
+		}catch(DuplicateNameException e) {
+			registerSuccess = false;
+			message = "Username " + username + " already exists";
 		}
 		return message;
 	}
 
 	@Override
-	public String login(String username, String password) throws PlayerNotFoundException {
-		login.loginPlayer(username, password);
-		return username + " has successfully logged in";
+	public String login(String username, String password) {
+		String message = username + " has successfully logged in";
+		try{
+			login.loginPlayer(username, password);
+			players.put(username, 0);
+		}catch(PlayerNotFoundException e) {
+			message = "Player with username " + username + "does not exist";
+		}
+		return message;
 	}
 
 	@Override
 	public String logout(String username) {
-		
-		return null;
+		boolean logoutSuccessful = login.logoutPlayer(username);
+		String message;
+		if(logoutSuccessful) {
+			message = username + " has successfully logged out.";
+		}else {
+			message = username + " was not succcessfully logged out";
+		}
+		return message;
 	}
 
 	@Override
@@ -140,18 +161,39 @@ public class GameEngineImpl implements GameEngine {
 		return board.getLegalMoves(position);
 	}
 	
-	public void reduceMoves() {
+	@Override
+	public Board getBoard() {
+		return board;
+	}
+	
+
+	@Override
+	public void setColr(String username, Colr colour) {
+		//TODO maybe implement an exception here if the player doesnt exists in the game?
+		if(players.containsKey(username)) {
+			playerColr.put(colour, username);
+		}	
+	}
+	
+	private void reduceMoves() {
 		currentMove--;
 	}
 
-	public void endGame() {
+	private void endGame() {
 		
 	}
-
-
-	@Override
-	public Board getBoard() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	private void increaseScore(int piecesTaken) {
+		String user = playerColr.get(currentTurn);
+		int currentScore = players.get(user);
+		if(piecesTaken==1) {
+			players.put(user, currentScore+5);
+		}else if(piecesTaken==2) {
+			players.put(user, currentScore+10);
+		}
+		//TODO
+		//view.updateScore();
 	}
+
+
 }
