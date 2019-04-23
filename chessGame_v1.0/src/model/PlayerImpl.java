84eb8c8346255bbcbc83d7c9d8c873b31c6bd441
s3716853 <41184784+s3716853@users.noreply.github.com>
2019-04-23 21:25:10 +1000

    Updated GameEngineImpl to more easily create new games

---
+++ chessGame_v1.0/src/model/PlayerImpl.java
@@ -0,0 +1,60 @@
+package model;
+
+import enums.Colr;
+import model_Interfaces.Player;
+
+/**
+ * Application of the Player Interface
+ * This class stores informaton related to players
+ * @author Matt Eletva
+ *
+ */
+
+public class PlayerImpl implements Player{
+	String name;
+	int points;
+	Colr colour;
+	
+	public PlayerImpl(String name){
+		this.name = name;
+		points = 0;
+		colour = null;
+	}
+	
+	
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	
+	@Override
+	public int getPoints() {
+		return points;
+	}
+
+	
+	@Override
+	public Colr getColour() {
+		// TODO Auto-generated method stub
+		return colour;
+	}
+	
+
+	@Override
+	public void increaseScore(int increaseAmount) {
+		points = points + increaseAmount;
+	}
+	
+
+	@Override
+	public void resetScore() {
+		points = 0;
+	}
+	
+
+	@Override
+	public void setColour(Colr colour) {
+		this.colour = colour;
+	}
+}
