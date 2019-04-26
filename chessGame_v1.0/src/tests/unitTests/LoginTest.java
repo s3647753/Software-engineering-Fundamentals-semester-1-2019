79a5e6e55788f2631e0944be5c1b685bec1e10a2
Speck <Speck@DESKTOP-PA0LFBA>
2019-04-26 17:19:07 +1000

    update LoginTest Author

--- chessGame_v1.0/src/tests/unitTests/LoginTest.java
+++ chessGame_v1.0/src/tests/unitTests/LoginTest.java
@@ -1,5 +1,11 @@
 package tests.unitTests;
 
+/**
+ * assortment of JUnit tests to verify that Login functions work correctly.
+ * 
+ * @author Shaun Davis
+ */
+
 import static org.junit.Assert.*;
 
 import org.junit.Before;
@@ -14,9 +20,9 @@ public class LoginTest {
 	private static final String TEST_PASS = "password";
 	private static final String TEST_NAME = "testman";
 	private static final String EXP_HASH = "5E884898DA28047151D0E56F8DC6292773603D0D6AABBDD62A11EF721D1542D8";
-	
+
 	private static RegisterLogin testLogin;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		testLogin = new RegisterLogin();
@@ -26,23 +32,24 @@ public class LoginTest {
 	public void testSHA() {
 		assertEquals("The hashes didn't match.", EXP_HASH, testLogin.stringToSHA256(TEST_PASS));
 	}
-	
+
 	@Test
 	public void testGetPlayerHash() throws PlayerNotFoundException {
-		// this test throws an exception. JUtil counts a test as a fail if any exception is thrown.
+		// this test throws an exception. JUtil counts a test as a fail if any exception
+		// is thrown.
 		assertEquals("The hash was retrieved incorrectly.", EXP_HASH, testLogin.getPlayerHash(TEST_NAME));
 	}
-	
+
 	@Test(expected = PlayerNotFoundException.class)
 	public void testPlayerNotFound() throws PlayerNotFoundException, WrongPassException {
 		testLogin.loginPlayer("me", "not a password");
 	}
-	
+
 	@Test(expected = WrongPassException.class)
 	public void testWrongPass() throws PlayerNotFoundException, WrongPassException {
 		testLogin.loginPlayer(TEST_NAME, "the wrong password!");
 	}
-	
+
 	@Test
 	public void testLogin() throws PlayerNotFoundException, WrongPassException {
 		testLogin.loginPlayer(TEST_NAME, TEST_PASS);
